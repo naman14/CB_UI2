48ea4df3955a19ad4a69ed9f970418d077e0b192
Naman Dwivedi <namandwivedi14@gmail.com>
2017-05-01 13:23:57 +0530

    circular reveal

--- app/src/main/java/com/naman14/cb_ui/MainActivity.java
+++ app/src/main/java/com/naman14/cb_ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.naman14.cb_ui;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.app.ActivityOptions;
 import android.content.Intent;
@@ -8,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +19,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ObjectAnimator animator = ObjectAnimator.ofFloat(findViewById(R.id.text1), "translationX", 0f, 700f);
-        animator.setDuration(1000);
-        animator.start();
+//        ObjectAnimator animator = ObjectAnimator.ofFloat(findViewById(R.id.text1), "translationX", 0f, 700f);
+//        animator.setDuration(1000);
+//        animator.start();
 
 
 //        Path path = new Path();
@@ -33,12 +36,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void navigate(View view) {
-        Intent intent= new Intent(this, SecondActivity.class);
+//        Intent intent= new Intent(this, SecondActivity.class);
+//
+//        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this,
+//                Pair.create(findViewById(R.id.image1), "transition_image"),
+//                Pair.create(findViewById(R.id.fab), "transition_fab"));
+//
+//        startActivity(intent, options.toBundle());
 
-        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this,
-                Pair.create(findViewById(R.id.image1), "transition_image"),
-                Pair.create(findViewById(R.id.fab), "transition_fab"));
+        ImageView imageView = (ImageView) findViewById(R.id.image1);
 
-        startActivity(intent, options.toBundle());
+        int cx = imageView.getWidth()/2;
+        int cy = imageView.getHeight()/2;
+
+        int finalradius = imageView.getHeight();
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(imageView,cx, cy,0, finalradius);
+
+        anim.setDuration(600);
+        imageView.setVisibility(View.VISIBLE);
+        anim.start();
+
     }
 }
