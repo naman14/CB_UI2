9aa5a482493e4bfe101603224a8f9ea6b8cb9ee5
Naman Dwivedi <namandwivedi14@gmail.com>
2017-05-01 12:42:59 +0530

    activity transition

--- app/src/main/java/com/naman14/cb_ui/MainActivity.java
+++ app/src/main/java/com/naman14/cb_ui/MainActivity.java
@@ -1,9 +1,13 @@
 package com.naman14.cb_ui;
 
 import android.animation.ObjectAnimator;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
 //        ObjectAnimator animator2 = ObjectAnimator.ofFloat(findViewById(R.id.text1), "x","y",path);
 //        animator.setDuration(1000);
 //        animator.start();
+    }
+
+    public void navigate(View view) {
+        Intent intent= new Intent(this, SecondActivity.class);
+
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this,
+                Pair.create(findViewById(R.id.image1), "transition_image"),
+                Pair.create(findViewById(R.id.fab), "transition_fab"));
+
+        startActivity(intent, options.toBundle());
     }
 }
